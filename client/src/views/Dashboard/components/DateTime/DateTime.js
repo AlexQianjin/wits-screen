d8f1cd8485eecd281eafc31bc1a5cf75f5ba0120
Jerry.Wang <picker.rry@outlook.com>
2023-08-29 16:22:41 +0800

    update wistronits logo

--- client/src/views/Dashboard/components/DateTime/DateTime.js
+++ client/src/views/Dashboard/components/DateTime/DateTime.js
@@ -8,101 +8,101 @@ import Time from './Time';
 import Date from './Date';
 
 const useStyles = makeStyles(theme => ({
-	root: {
-		height: '100%'
-	},
-	content: {
-		alignItems: 'center',
-		display: 'flex'
-	},
-	title: {
-		fontWeight: 700
-	},
-	avatar: {
-		backgroundColor: theme.palette.error.main,
-		height: 56,
-		width: 56
-	},
-	icon: {
-		marginTop: 34,
-		marginLeft: 48
-	},
-	difference: {
-		marginTop: theme.spacing(2),
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		textAlign: 'center',
-		height: 'auto'
-	},
-	differenceIcon: {
-		color: theme.palette.error.dark
-	},
-	differenceValue: {
-		color: theme.palette.error.dark,
-		marginRight: theme.spacing(1)
-	},
-	beijingTime: {
-		marginTop: 48,
-		marginLeft: 40
-	}
+    root: {
+        height: '100%'
+    },
+    content: {
+        alignItems: 'center',
+        display: 'flex'
+    },
+    title: {
+        fontWeight: 700
+    },
+    avatar: {
+        backgroundColor: theme.palette.error.main,
+        height: 56,
+        width: 56
+    },
+    icon: {
+        marginTop: 10,
+        marginLeft: 10,
+        height: 100,
+        width: 460
+    },
+    difference: {
+        marginTop: theme.spacing(2),
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        height: 'auto'
+    },
+    differenceIcon: {
+        color: theme.palette.error.dark
+    },
+    differenceValue: {
+        color: theme.palette.error.dark,
+        marginRight: theme.spacing(1)
+    },
+    beijingTime: {
+        marginTop: 48,
+        marginLeft: 40
+    }
 }));
 
 const DateTime = props => {
-	const { className, ...rest } = props;
+    const { className, ...rest } = props;
 
-	const classes = useStyles();
+    const classes = useStyles();
 
-	return (
-		<Card
-			{...rest}
-			className={clsx(classes.root, className)}
-		>
-			<CardContent style={{padding: 0}}>
-				<Grid
-					container
-					justifyContent="space-between"
-				>
-					<Grid item>
-						<CardMedia
-							component="img"
-							alt="Wistron ITS"
-							image="./images/img_logo.png"
-							title="Wistron ITS"
-							className={classes.icon}
-						></CardMedia>
-					</Grid>
-				</Grid>
-				<div
-					className={classes.difference}
-					style={{width: '553px', height: '358px', backgroundImage: 'url("./images/img_bg_time.png")', marginTop: '78px', marginLeft: '104px'}}
-				>
-					<Time />
-					<div>
-						<Date/>
-					</div>
-				</div>
-				<Grid
-					container
-					justifyContent="space-between"
-				>
-					<Grid item>
-						<CardMedia
-							component="img"
-							alt="Beijing Time"
-							image="./images/img_leftblock.png"
-							title="Beijing Time"
-							className={classes.beijingTime}
-						></CardMedia>
-					</Grid>
-				</Grid>
-			</CardContent>
-		</Card>
-	);
+    return (
+        <Card {...rest} className={clsx(classes.root, className)}>
+            <CardContent style={{ padding: 0 }}>
+                <Grid container justifyContent="space-between">
+                    <Grid item>
+                        <CardMedia
+                            component="img"
+                            alt="Wistron ITS"
+                            image="./images/img_logo.png"
+                            title="Wistron ITS"
+                            className={classes.icon}
+                        ></CardMedia>
+                    </Grid>
+                </Grid>
+                <div
+                    className={classes.difference}
+                    style={{
+                        width: '553px',
+                        height: '358px',
+                        backgroundImage: 'url("./images/img_bg_time.png")',
+                        marginTop: '78px',
+                        marginLeft: '104px'
+                    }}
+                >
+                    <Time />
+                    <div>
+                        <Date />
+                    </div>
+                </div>
+                <Grid container justifyContent="space-between">
+                    <Grid item>
+                        <CardMedia
+                            component="img"
+                            alt="Beijing Time"
+                            image="./images/img_leftblock.png"
+                            title="Beijing Time"
+                            className={classes.beijingTime}
+                        ></CardMedia>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
 };
 
 DateTime.propTypes = {
-	className: PropTypes.string
+    className: PropTypes.string
 };
 
 export default DateTime;
+
